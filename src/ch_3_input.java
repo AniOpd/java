3e3f3425300136a878cf2a864378c9e4f96d4b8a
Anish <Anishd988@gmail.com>
2022-07-16 22:21:15 +0530

    input taking

---
+++ src/ch_3_input.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class ch_3_input {
+    public static void  main(String[] arg){
+        System.out.println("Enter the number");
+        Scanner sc =new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.println("The number is "+a);
+    }
+}
