9f1a43efcf9be6c530b6a46f65cb8e02be052915
Anish <Anishd988@gmail.com>
2022-07-16 22:41:58 +0530

    percentage calculator

---
+++ src/percentage_calculator.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class percentage_calculator {
+    public static void main(String[] arg){
+        System.out.println("Enter the marks of maths");
+        Scanner sc=new Scanner(System.in);
+        double m= sc.nextDouble();
+        System.out.println("Enter the marks of Science");
+        double s= sc.nextDouble();
+        System.out.println("Enter the marks of English");
+        double E= sc.nextDouble();
+        double P=((m+s+E)/300)*100;
+        System.out.println("The percentage of the student is :-");
+        System.out.println(P);
+    }
+}
