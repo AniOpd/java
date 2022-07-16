00c6d6ba3c54204b12500b6084671c7ff49309bc
Anish <Anishd988@gmail.com>
2022-07-16 22:31:29 +0530

    input taking

--- src/ch_3_input.java
+++ src/ch_3_input.java
@@ -1,9 +1,16 @@
 import java.util.Scanner;
 public class ch_3_input {
     public static void  main(String[] arg){
+       Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the string");
+        String str=sc.nextLine();
+        System.out.println(str);
         System.out.println("Enter the number");
-        Scanner sc =new Scanner(System.in);
         int a=sc.nextInt();
         System.out.println("The number is "+a);
+        System.out.println("Another number");
+        boolean b1= sc.hasNextInt();
+        System.out.println(b1);
+
     }
 }
