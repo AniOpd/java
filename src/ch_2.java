fade34b4e55deea478ad6b9dae7f4fa471c2640e
Anish <Anishd988@gmail.com>
2022-07-16 22:10:12 +0530

    litrels

---
+++ src/ch_2.java
@@ -0,0 +1,14 @@
+public class ch_2 {
+    public static void main(String[] arg){
+        System.out.println("start");
+        byte a=32;
+        int b=56;
+        char c='a';
+        float k=544.654f;
+        double d=1132;
+        boolean g=true;
+        String str="Opakki";
+        System.out.println(str);
+
+    }
+}
