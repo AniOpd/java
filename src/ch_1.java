94a0b30b617dd015b840d78043bf8082bf7fe67e
Anish <Anishd988@gmail.com>
2022-07-16 21:50:15 +0530

    day 1

---
+++ src/ch_1.java
@@ -0,0 +1,11 @@
+public class ch_1 {
+    public static void main(String[] arg){
+        System.out.print("The sum of four numbers:-\n");
+        int a,b,c,d;
+        a=5;b=8;c=75;d=23;
+        int sum;
+        sum=a+b+c+d;
+        System.out.println(sum);
+
+    }
+}
