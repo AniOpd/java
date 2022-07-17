ea573a0b1940f0a524d8062b6c56f406d42af87d
Anish <Anishd988@gmail.com>
2022-07-17 09:29:07 +0530

    Q-3

---
+++ src/q_3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class q_3 {
+    public static void main(String[] arg) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Name of the user:-");
+        String str = sc.nextLine();
+        System.out.println(str + " Have a Good day!");
+
+    }
+}
