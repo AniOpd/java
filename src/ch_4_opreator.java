e424cc20f7dabb84487f6a84b723e171d5f2c7bc
Anish <Anishd988@gmail.com>
2022-07-17 19:56:15 +0530

    operator

---
+++ src/ch_4_opreator.java
@@ -0,0 +1,13 @@
+public class ch_4_opreator {
+    public static void main(String[] arg){
+        // assignment opretor
+        int b=9;
+        b+=6;
+        System.out.println(b);
+        //logical opreator
+        System.out.println(89>88 && 89<78);
+        System.out.println(89<90 || 89>78);
+        //bitwise opreator
+        System.out.println(2&3);
+    }
+}
