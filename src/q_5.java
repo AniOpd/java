3c12a0f7d5925c2c51ac6c2988882e64ef73e0d4
Anish <Anishd988@gmail.com>
2022-07-17 09:40:06 +0530

    Q-5

---
+++ src/q_5.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class q_5 {
+    public static void main(String[] arg){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number to check");
+        boolean a;
+        a=sc.hasNextInt();
+        System.out.println("The entered value is int :- "+ a);
+    }
+}
