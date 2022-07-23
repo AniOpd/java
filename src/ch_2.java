59df7b2a4b967852c1dff87322fc00d7193260b3
Anish <Anishd988@gmail.com>
2022-07-23 19:40:41 +0530

    string questions

--- src/ch_2.java
+++ src/ch_2.java
@@ -1,13 +1,13 @@
 public class ch_2 {
-    public static void main(String[] arg){
+    public static void main(String[] arg) {
         System.out.println("start");
-        byte a=32;
-        int b=56;
-        char c='a';
-        float k=544.654f;
-        double d=1132;
-        boolean g=true;
-        String str="Opakki";
+        byte a = 32;
+        int b = 56;
+        char c = 'a';
+        float k = 544.654f;
+        double d = 1132;
+        boolean g = true;
+        String str = "Opakki";
         System.out.println(str);
 
     }
