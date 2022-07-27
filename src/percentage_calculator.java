ff4379f264fd85e2efd5dcad4c318499b18db0cf
Anish <Anishd988@gmail.com>
2022-07-27 22:47:45 +0530

    pattern question

--- src/percentage_calculator.java
+++ src/percentage_calculator.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
+
 public class percentage_calculator {
-    public static void main(String[] arg){
+    public static void main(String[] arg) {
         System.out.println("Enter the marks of maths");
-        Scanner sc=new Scanner(System.in);
-        double m= sc.nextDouble();
+        Scanner sc = new Scanner(System.in);
+        double m = sc.nextDouble();
         System.out.println("Enter the marks of Science");
-        double s= sc.nextDouble();
+        double s = sc.nextDouble();
         System.out.println("Enter the marks of English");
-        double E= sc.nextDouble();
-        double P=((m+s+E)/300)*100;
+        double E = sc.nextDouble();
+        double P = ((m + s + E) / 300) * 100;
         System.out.println("The percentage of the student is :-");
         System.out.println(P);
     }
